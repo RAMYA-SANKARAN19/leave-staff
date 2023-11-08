0541890fcc34b29611f1ded8c079bfc8f9d4a462
unknown <ramyasankaran19@gmail.com>
2023-11-08 15:20:27 +0530

    first commit

---
+++ heads/includes/footer.php
@@ -0,0 +1,5 @@
+<div class="footer-wrap pd-20 mb-20 card-box">
+				<!-- ACI Leave System <a href="https://aci.edu.gh/" target="_blank"><span>developed by </span> ACI ICT</a> -->
+				
+Leave Management System &copy; copyrights reserved
+			</div>
---
+++ staff/includes/footer.php
@@ -0,0 +1,4 @@
+<div class="footer-wrap pd-20 mb-20 card-box">
+				Leave Management System &copy; copyrights reserved
+				<!-- <a href="https://aci.edu.gh/" target="_blank"><span>developed by </span> ACI ICT</a> -->
+			</div>
